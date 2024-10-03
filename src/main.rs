77a831e70148782461d241462de2c10cdb6eb6a6
Sean McConnachie <seanjulian.mcc@gmail.com>
2024-10-04 12:10:36 +1300

    Better error for moreutils package

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
-fn main() -> anyhow::Result<()> {
+use anyhow::Result;
+
+fn is_vipe_installed() -> Result<()> {
+    use std::process::Command;
+    if !Command::new("which").arg("vipe").output()?.status.success() {
+        eprintln!("vipe is not installed. Please install `moreutils` package.");
+        std::process::exit(1);
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    is_vipe_installed()?;
+
     use clap::Parser;
     use secure_edit::cli;
 
